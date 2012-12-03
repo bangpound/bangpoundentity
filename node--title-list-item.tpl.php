d695185030642e99eed48ae6bca9161e888a8f9a
Benjamin Doherty <bjd@pobox.com>
2012-12-02 22:43:47 -0600

    improve title list item view mode theme.

--- node--title-list-item.tpl.php
+++ node--title-list-item.tpl.php
@@ -83,6 +83,12 @@
  * @see template_process()
  */
 ?>
+<?php if ($wrapper): ?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<?php endif; ?>
 <?php print render($title_prefix); ?>
 <a href="<?php print $node_url; ?>"<?php print $title_attributes; ?>><?php print $title; ?></a>
 <?php print render($title_suffix); ?>
+<?php if ($wrapper): ?>
+</div>
+<?php endif; ?>
